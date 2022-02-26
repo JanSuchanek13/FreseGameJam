921899d97c599566c95de8a4a1c386272064270d
JanSuchanek13 <82054660+JanSuchanek13@users.noreply.github.com>
2022-02-26 02:01:03 +0100

    Update Jan
    Third person Move now with Gravity, GroundCheck and Jumping
    New Layer Ground

--- FreseGameJam/Assets/Scripts/ThirdPersonMovement.cs
+++ FreseGameJam/Assets/Scripts/ThirdPersonMovement.cs
@@ -12,9 +12,20 @@ public class ThirdPersonMovement : MonoBehaviour
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+
+    Vector3 velocity;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
     // Update is called once per frame
     void Update()
     {
+        //Camera and Movement
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -27,6 +38,30 @@ public class ThirdPersonMovement : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+
+           
         }
+
+        //Check Ground
+        //controller = GetComponent<CharacterController>();
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            controller.slopeLimit = 45.0f;
+            velocity.y = -2f;
+        }
+
+        //Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            controller.slopeLimit = 100f;
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        //Gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
