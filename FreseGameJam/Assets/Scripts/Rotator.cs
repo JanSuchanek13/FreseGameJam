7576e12d344bcbc3335760438e34780ea9319f2f
susi2301 <susannebataloff@gmail.com>
2022-02-27 18:42:17 +0100

    update susi
    - hoverer script
    -rotator script

---
+++ FreseGameJam/Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] private Vector3 rotationVector = Vector3.left;
+    [SerializeField] private float speed = 1f;
+
+    void Update()
+    {
+        transform.Rotate(rotationVector, speed * Time.deltaTime, Space.World);
+    }
+
+}
+
