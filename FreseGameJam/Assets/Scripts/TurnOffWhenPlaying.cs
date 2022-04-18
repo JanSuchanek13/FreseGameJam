4e4b6a889a30c3e072af0bced052de0590a95b94
Felix <f.mundschenk@web.de>
2022-04-19 00:11:55 +0200

    Felix Update
    - cleaned up editor hirarchy of lvl1
    - small changes to level
    - tried to clean up camera track by removing "ground" tag & layer, but found issue to be colliders - cannot remove those. made ticket for JAN to switch avoidance from Colliders to TAG or LAYER
    - made "Bridge" Tag and triggerbox to make steep bridges
    - implemented UI & crown counter --> simply drag TMPro-part into your coutner-code @Jan
    - fixed my TurnOffWhenPlaying script to turn off renderer, instead object (to keep logic running)
    - test-material bright pink transluscent to flag triggers in map that get turnt off when playing (visual only)

--- FreseGameJam/Assets/Scripts/TurnOffWhenPlaying.cs
+++ FreseGameJam/Assets/Scripts/TurnOffWhenPlaying.cs
@@ -7,6 +7,7 @@ public class TurnOffWhenPlaying : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.gameObject.SetActive(false);
+        //this.gameObject.SetActive(false);
+        this.gameObject.GetComponent<MeshRenderer>().enabled = false;
     }
 }
