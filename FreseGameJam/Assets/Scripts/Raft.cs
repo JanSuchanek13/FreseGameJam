d4313edfd955fc7c0e18d652b3e317e1bbcacbf8
JanSuchanek13 <82054660+JanSuchanek13@users.noreply.github.com>
2022-05-12 19:48:48 +0200

    Update Jan
    Raft script

---
+++ FreseGameJam/Assets/Scripts/Raft.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Raft : MonoBehaviour
+{
+	public bool started = false;
+
+
+	public Transform[] points;
+	private int destPoint = 0;
+	private NavMeshAgent agent;
+	private float obstacleAvoidance;
+	private ObstacleAvoidanceType type;
+	private float height;
+	private float speed;
+
+
+
+
+	private void Start()
+	{
+
+
+		agent = GetComponent<NavMeshAgent>();
+
+		obstacleAvoidance = agent.radius;
+		type = agent.obstacleAvoidanceType;
+		height = agent.height;
+		speed = agent.speed;
+		//Debug.Log(obstacleAvoidance);
+
+
+		// Disabling auto-braking allows for continuous movement
+		// between points (ie, the agent doesn't slow down as it
+		// approaches a destination point).
+		agent.autoBraking = false;
+
+		
+
+	}
+
+	void GotoNextPoint()
+	{
+		// Returns if no points have been set up
+		if (points.Length == 0)
+		{
+
+			return;
+		}
+
+
+
+		// Set the agent to go to the currently selected destination.
+		agent.destination = points[destPoint].position;
+
+		// Choose the next point in the array as the destination,
+		// cycling to the start if necessary.
+		destPoint = (destPoint + 1) % points.Length;
+
+		
+		
+
+	}
+
+	void FixedUpdate()
+	{
+		/*
+		currentPos = Vector3.Lerp(startPoint.position, endPoint.position,
+			Mathf.Cos(Time.time / travelTime * Mathf.PI * 2) * -.5f + .5f);
+		rb.MovePosition(currentPos);
+		*/
+
+
+
+
+
+		// Choose the next destination point when the agent gets
+		// close to the current one.
+		
+		if (started)
+		{
+			if (!agent.pathPending && agent.remainingDistance < 2f)
+				GotoNextPoint();
+		}
+
+
+
+	}
+	private void OnTriggerEnter(Collider other)
+	{
+		if (started == false && other.tag == "Player")
+        {
+			started = true;
+			GotoNextPoint();
+		}
+		
+		
+
+
+
+	}
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			//cc.Move(rb.velocity * Time.deltaTime);
+			//cc.Move(transform.position.normalized * Time.deltaTime*3.5f );
+			other.transform.parent = transform;
+		}
+
+	}
+	private void OnTriggerExit(Collider other)
+	{
+		
+		if (other.tag == "Player")
+		{
+			other.transform.parent = null;
+			
+		}
+
+
+
+	}
+
+	
+}
