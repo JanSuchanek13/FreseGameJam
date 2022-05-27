2e8293dd24e3b60cade240505c072a1367ea2aa7
susi2301 <susannebataloff@gmail.com>
2022-05-27 21:27:25 +0200

    update susi
    added read me file -> needs to be filled!
    deleted not free fonts added new ones
    added standard cardboard panel

---
+++ FreseGameJam/Assets/Scripts/ToTextFile.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class ToTextFile : MonoBehaviour
+{
+    public InputField inputFieldChat;
+
+
+   void Start()
+    {
+        Directory.CreateDirectory(Application.streamingAssetsPath + "/Chat_Logs/");
+
+    }
+
+
+    public void CreateTextFile()
+    {
+
+        if (inputFieldChat.text == "")
+        {
+            return;
+        }
+
+        string txtDocumentName = Application.streamingAssetsPath + "/Chat_Logs/" + "Chat" + ".txt";
+
+        if (!File.Exists(txtDocumentName))
+        {
+            File.WriteAllText(txtDocumentName, "Title of my chat log \n\n");
+
+        }
+
+        File.AppendAllText(txtDocumentName, inputFieldChat.text + "/n");
+
+        inputFieldChat.text = "";
+    }
+
+
+}
---
+++ ReadMeCredits.txt
@@ -0,0 +1,18 @@
+Textures:
+
+
+
+Fonts:
+-Paper Cuts by Empire of Dust (Public Domain)
+
+
+Sounds: 
+
+
+
+
+
+Tools:
+
+ReplaceWithPrefab : https://unity3d.college/2017/09/07/replace-gameobjects-or-prefabs-with-another-prefab/
+
