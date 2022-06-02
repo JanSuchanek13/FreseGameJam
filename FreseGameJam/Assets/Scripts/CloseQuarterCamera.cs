bcfad33dec26f02cbad5d56106145c842eeaca72
Felix <f.mundschenk@web.de>
2022-06-02 11:27:29 +0200

    Felix Update
    - built camera-switch zones, works pretty good (lvl1)
    - massive bugs with invis. colliders by Jans jump-script --> talked about it, he will fix (level not playable until then)
    - new Tag for camera switch
    - added script to player prefab to adjust cameras & attached secondary camera rig!
    - new stairs behidn lava-falls to avoid jiggedyjigged camerajumps
    - more destructable bridges
    - more subclasses of structures on vulcano island

--- FreseGameJam/Assets/Scripts/CloseQuarterCamera.cs
+++ FreseGameJam/Assets/Scripts/CloseQuarterCamera.cs
@@ -1,50 +1,135 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Cinemachine;
 
 public class CloseQuarterCamera : MonoBehaviour
 {
-    //[SerializeField] float newCameraMaxDistance_top = 8;
-    //[SerializeField] float newCameraMaxDistance_middle = 8;
+    #region variables
+    private GameObject _lastTriggerHit;
+    private GameObject _cameraRigFar;
+    private bool _closeQuarterCameraIsActive = false;
+    private bool _currentlyInsideTrigger = false;
+    #endregion
+    void Start()
+    {
+        _cameraRigFar = this.transform.GetChild(0).gameObject;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("CloseQuarterCameraTrigger") && !_currentlyInsideTrigger)
+        {
+            _lastTriggerHit = other.gameObject;
+            _lastTriggerHit.GetComponent<BoxCollider>().enabled = false;
+            Invoke("ReactivateTrigger", 1f);
+            _currentlyInsideTrigger = true; // redundant here, as turning on/off the collider solves the same issue.
+
+            if (!_closeQuarterCameraIsActive)
+            {
+                ZoomIn();
+            }else
+            {
+                ZoomOut();
+            }
+        }
+    }
+    void ZoomIn()
+    {
+        _cameraRigFar.SetActive(false);
+        _closeQuarterCameraIsActive = true;
+        Debug.Log("zoom in");
+    }
+    void ZoomOut()
+    {
+        _cameraRigFar.SetActive(true);
+        _closeQuarterCameraIsActive = false;
+        Debug.Log("zoom out");
+    }
+
+    void ReactivateTrigger()
+    {
+        _currentlyInsideTrigger = false; // redundant here, as turning on/off the collider solves the same issue.
+        _lastTriggerHit.GetComponent<BoxCollider>().enabled = true;
+    }
+    private void Update() // failsafe to enable zones after dying!
+    {
+        if (GetComponent<HealthSystem>().inUse)
+        {
+            Debug.Log("I died and am resetting camera");
+            ZoomOut();
+            ReactivateTrigger();
+        }
+    }
+}
+
+    //this was beautifull but i made one mistake... sitting on a trigger they always thought they were the first one hit...
+    /*
+    #region variables
     private GameObject _player;
     private GameObject _cameraRigFar;
-    //private GameObject _cameraRigClose;
-
-
-    public bool _closeQuarterCameraIsActive = false;
-    public bool _currentlyInsideTrigger = false;
+    //private GameObject _cameraRigClose; // not needed as its hirarchy placement seems to be enough to fit in snug with camera logic (far, if far is available, otherwise close!)
+    private bool _closeQuarterCameraIsActive = false;
+    private bool _currentlyInsideTrigger = false;
+    #endregion
 
     void Start()
     {
         _player = GameObject.Find("Third Person Player");
         _cameraRigFar = _player.transform.GetChild(0).gameObject;
-        //_cameraRigClose = _player.transform.GetChild(1).gameObject;
+        //_cameraRigClose = _player.transform.GetChild(1).gameObject; // redundancy *see above*
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !_currentlyInsideTrigger)
         {
-            _currentlyInsideTrigger = true;
+            GetComponent<BoxCollider>().enabled = false;
+            Invoke("ReactivateTrigger", 1f);
+            _currentlyInsideTrigger = true; // redundant here, as turning on/off the collider solves the same issue.
 
             if (!_closeQuarterCameraIsActive)
             {
-                _cameraRigFar.SetActive(false);
-                _closeQuarterCameraIsActive = true;
+                ZoomIn();
             }
             else
             {
-                _cameraRigFar.SetActive(true);
-                _closeQuarterCameraIsActive = false;
+                ZoomOut();
             }
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    /*private void OnTriggerExit(Collider other) // this is much more elegant but does not seem to work with a sphere/mesh-collider that is on the Player Avatar (box should work with this, but to much extra work)
     {
         if (other.CompareTag("Player"))
         {
             _currentlyInsideTrigger = false;
         }
     }
-}
+    void ZoomIn()
+    {
+        _cameraRigFar.SetActive(false);
+        _closeQuarterCameraIsActive = true;
+        Debug.Log("zoom in");
+    }
+    void ZoomOut()
+    {
+        _cameraRigFar.SetActive(true);
+        _closeQuarterCameraIsActive = false;
+        Debug.Log("zoom out");
+    }
+
+    void ReactivateTrigger()
+    {
+        _currentlyInsideTrigger = false; // redundant here, as turning on/off the collider solves the same issue.
+        GetComponent<BoxCollider>().enabled = true;
+    }
+
+    private void Update() // failsafe to enable zones after dying!
+    {
+        if (_player.GetComponent<HealthSystem>().inUse)
+        {
+            Debug.Log("I died");
+            ZoomOut();
+            _currentlyInsideTrigger = false; // redundant here, as turning on/off the collider solves the same issue.
+            GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+}*/
