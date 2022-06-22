ceb806ef8d81aa50eae7bc11b2c5e80aeb75f99a
Felix <f.mundschenk@web.de>
2022-06-22 20:34:23 +0200

    Felix Update
    - added all Button Functions to "ButtonFunction" script on game manager (save & restore settings are dummies for now)
    - new run in start menu now resets the game as intended again
    --> All buttons in the game now give feedback to player, even if no real effect! yay

--- FreseGameJam/Assets/Scripts/ButtonFunction.cs
+++ FreseGameJam/Assets/Scripts/ButtonFunction.cs
@@ -5,18 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class ButtonFunction : MonoBehaviour
 {
+    #region variables:
     float[] lastTimer = new float[3];
     int currentLevel;
-    int pingPongAlpha = 1;
+    //int pingPongAlpha = 1;
     public CanvasGroup UI;
 
+    [SerializeField] GameObject pauseMenu;
+    #endregion
 
     private void Start()
     {
-        //safe level
+        // save level:
         currentLevel = SceneManager.GetActiveScene().buildIndex;
 
-        //add time of last run
+        // add time of last run:
         lastTimer[(currentLevel - 2)] = PlayerPrefs.GetFloat("lastTimer" + (currentLevel - 2));
     }
 
@@ -27,11 +30,47 @@ public class ButtonFunction : MonoBehaviour
 
         if (Input.GetButtonDown("Cancel"))
         {
-            Debug.Log("pressed ESC");
-            UI.alpha = pingPongAlpha;
-            pingPongAlpha = pingPongAlpha * -1;
+            //Debug.Log("pressed ESC");
+            //UI.alpha = pingPongAlpha;
+            //pingPongAlpha = pingPongAlpha * -1;
+            Pause();
+        }
+    }
+    public void Pause()
+    {
+        if (pauseMenu.activeInHierarchy != true)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }else
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
         }
     }
 
-    
+    public void BackToMain()
+    {
+        Debug.Log("Back to Main pressed!");
+        Time.timeScale = 1; // otherwise "continue"-button will start the game paused. Unless we want that?
+        SceneManager.LoadScene(0);
+    }
+
+    public void Exit()
+    {
+        Debug.Log("application would now be closed!");
+        Time.timeScale = 1; // otherwise "continue"-button will start the game paused. Unless we want that?
+        Application.Quit();
+    }
+
+    public void SaveNewSettings()
+    {
+        Debug.Log("you saved your new settings!");
+        // save stuff now
+    }
+    public void SettingsToDefault()
+    {
+        Debug.Log("you restored the default settings!");
+        // restore default settings now
+    }
 }
