9e9724040b7474a8fe0b650b658a4879af240ffa
Susi <susannebataloff@gmail.com>
2023-01-06 19:33:01 +0100

    update susi
    - worked on shadowscript
       - changed that raycasts affects all layers also ignore raycast
       - changed the quaternion rotation berechnung -- plane parrallel auf oberfläche projezieren
          lassen
    
    - last thing was to fix whack colliders where the shadow plane didnt work , theser were:
    
      - water prefab -> added collider to wavez
      - stone prefab -> repositioned stone cangled collider
      - grasshalme 1 und 2 prefab -> großer collider was too big

--- FreseGameJam/Assets/Scripts/CharacterPlayerController/Shadow.cs
+++ FreseGameJam/Assets/Scripts/CharacterPlayerController/Shadow.cs
@@ -10,6 +10,8 @@ public class Shadow : MonoBehaviour
     // Referenz auf das GameObject mit dem Decal Projector
     public GameObject shadowPlane;
 
+    int layerMask = ~0; // all layers
+
 
 
     void Update()
@@ -20,22 +22,30 @@ public class Shadow : MonoBehaviour
         RaycastHit hit;
 
         // Führe den Raycast aus und speichere das Ergebnis in der hit-Variable
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
         {
             // Wenn der Raycast einen Treffer hat, setze die Y-Position der Schattenplane auf den Y-Wert des Treffers
-            shadowPlane.transform.position = new Vector3(player.transform.position.x, hit.point.y + 0.02f, player.transform.position.z);
+            shadowPlane.transform.position = new Vector3(player.transform.position.x, hit.point.y + 0.01f, player.transform.position.z);
+
+
+               
 
             // Hole den Normalenvektor des Treffers (der zeigt nach oben vom Boden)
             Vector3 normal = hit.normal;
 
+            shadowPlane.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+
+            /*
+
             // Berechne den Winkel des Bodens in Grad
-            float angle = Vector3.Angle(normal, Vector3.up);
-
-   
-
+           float angle = Vector3.Angle(normal, Vector3.up);
 
             // Setze die Rotation der Schattenplane auf eine neue Quaternion-Rotation, die auf den Winkel des Bodens abgestimmt ist
             shadowPlane.transform.rotation = Quaternion.Euler(0, 0, angle);
+
+            */
+
+            
 
         }
     }
