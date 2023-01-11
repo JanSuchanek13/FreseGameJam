3f3a1bb1ca60a6cceedadfa56369e0c34803c860
JanSuchanek13 <82054660+JanSuchanek13@users.noreply.github.com>
2023-01-11 14:58:37 +0100

    Update Jan
    new script ChangeControlsSign
    Sign got temp keyboard and Ps4 Controlvisuell

---
+++ FreseGameJam/Assets/Scripts/UI/ChangeControlsSign.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeControlsSign : MonoBehaviour
+{
+    [Header("REFERENCE")]
+    public GameObject keyboardControls;
+    public GameObject ps4Controls;
+
+    [Header("REFERENCE")]
+    [Tooltip("0=Keyboard, 1=Ps4")]
+    public int controlSettings = 0;
+    void Awake()
+    {
+        switch (controlSettings)
+        {
+            case 0:
+                keyboardControls.SetActive(true);
+                break;
+
+            case 1:
+                ps4Controls.SetActive(true);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+}
