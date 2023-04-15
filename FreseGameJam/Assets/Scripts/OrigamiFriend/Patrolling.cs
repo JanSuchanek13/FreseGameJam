7216b91de15b4622c5c0e52f6f1597c05665fbe7
Felix <f.mundschenk@web.de>
2023-04-15 09:34:12 +0200

    Update Felix
    - friend in boat now always driving (unless waiting)
    - if he finished his travel he will now spawn directly at level end
    - pressing continue along the way will make the fren travel along its last known position!
    - moved crown island closer to vulcano to bea reachable

--- FreseGameJam/Assets/Scripts/OrigamiFriend/Patrolling.cs
+++ FreseGameJam/Assets/Scripts/OrigamiFriend/Patrolling.cs
@@ -5,17 +5,23 @@ using System.Collections;
 
 public class Patrolling : MonoBehaviour
 {
+    // public variables:
+    public Transform[] arrayOfCheckpoints;
 
-    public Transform[] points;
-    private int destPoint = 0;
-    private NavMeshAgent agent;
-    bool ReachedTarget = false;
+    // private variables:
+    private NavMeshAgent _navMeshAgent;
+
+    private bool _reachedTarget = false;
+    private bool _waitingForPlayer = false;
+
     [SerializeField] GameObject _turnOffElements;
     [SerializeField] GameObject _turnOnElements;
-    private bool _waitingForPlayer = false;
+
     private float _defaultSpeed;
+
     // save current position of boat:
-    int _boatPosition = 0;
+    private int _destPoint = 0;
+    private int _boatPosition = 0;
 
     void Start()
     {
@@ -25,49 +31,45 @@ public class Patrolling : MonoBehaviour
             _boatPosition = PlayerPrefs.GetInt("_boatPosition"); // update _boatPosition:
             if (PlayerPrefs.GetInt("_boatPosition") != 0)
             {
-                destPoint = PlayerPrefs.GetInt("_boatPosition") + 1;
-                this.transform.position = points[PlayerPrefs.GetInt("_boatPosition")].transform.position;
+                _destPoint = PlayerPrefs.GetInt("_boatPosition") + 1;
+                this.transform.position = arrayOfCheckpoints[PlayerPrefs.GetInt("_boatPosition")].transform.position;
             }
 
-            agent = GetComponent<NavMeshAgent>();
+            _navMeshAgent = GetComponent<NavMeshAgent>();
 
             // Disabling auto-braking allows for continuous movement
             // between points (ie, the agent doesn't slow down as it
             // approaches a destination point).
-            agent.autoBraking = false;
-            _defaultSpeed = agent.speed;
+            _navMeshAgent.autoBraking = false;
+            _defaultSpeed = _navMeshAgent.speed;
             GotoNextPoint();
         }else
         {
-            ReachedTarget = true;   
+            RelocateBoat(); 
         }
     }
-
-    // save if cutscene has played:
-    // _cutSceneHasAlreadyPlayed++;
-    //        PlayerPrefs.SetInt("_cutSceneHasAlreadyPlayed", _cutSceneHasAlreadyPlayed);
 
     void GotoNextPoint()
     {
 
-        if (ReachedTarget == false && !_waitingForPlayer)
+        if (_reachedTarget == false && !_waitingForPlayer)
         {
             // Returns if no points have been set up
-            if (points.Length == 0)
+            if (arrayOfCheckpoints.Length == 0)
                 return;
 
             // Set the agent to go to the currently selected destination.
-            agent.destination = points[destPoint].position;
+            _navMeshAgent.destination = arrayOfCheckpoints[_destPoint].position;
 
             // Choose the next point in the array as the destination,
             // cycling to the start if necessary.
-            destPoint = (destPoint + 1) % points.Length;
+            _destPoint = (_destPoint + 1) % arrayOfCheckpoints.Length;
 
             // save boat position:
             SaveBoatPosition();
         }else
         {
-            agent.enabled = false;
+            _navMeshAgent.enabled = false;
         }
     }
 
@@ -76,15 +78,19 @@ public class Patrolling : MonoBehaviour
     {
         // Choose the next destination point when the agent gets
         // close to the current one.
-        if (!ReachedTarget && !_waitingForPlayer)
+        if (!_reachedTarget && !_waitingForPlayer)
         {
-            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            if (!_navMeshAgent.pathPending && _navMeshAgent.remainingDistance < 0.5f)
+            {
                 GotoNextPoint();
-        }else
+            }
+        }
+        else
         {
             enabled = false;
         }
     }
+
     void SaveBoatPosition()
     {
         PlayerPrefs.SetInt("_boatPosition", _boatPosition);
@@ -94,31 +100,29 @@ public class Patrolling : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Destination") && !ReachedTarget)
+        if (other.CompareTag("Destination") && !_reachedTarget)
         {
-            ReachedTarget = true;
-
-            // get rid of bug about having waypoints out of array:
-            PlayerPrefs.SetInt("_reachedEndOfTravel", 1);
-
-            //Debug.Log("I reached my destination!!!!");
-            //_turnOffElements.SetActive(false);
-            _turnOnElements.SetActive(true);
-            agent.isStopped = true;
-            _turnOffElements.SetActive(false);
+            RelocateBoat();
         }
     }
+    void RelocateBoat()
+    {
+        // get rid of bug about having waypoints out of array:
+        PlayerPrefs.SetInt("_reachedEndOfTravel", 1);
+        
+        _reachedTarget = true;
+        
+        _turnOnElements.SetActive(true);
+        _turnOffElements.SetActive(false);
+    }
 
-
-    // pollish: this makes friend wait when touching trigger at a given location, needs a wake-up-trigger to call "StopWaiting()".
+    // polish: this makes friend wait when touching trigger at a given location, needs a wake-up-trigger to call "StopWaiting()".
     public void WaitForPlayer()
     {
-        agent.speed = 0;
-        //Debug.Log("now waiting for player");
+        _navMeshAgent.speed = 0;
     }
     public void StopWaiting()
     {
-        agent.speed = _defaultSpeed;
-        //Debug.Log("continuing travel");
+        _navMeshAgent.speed = _defaultSpeed;
     }
 }
