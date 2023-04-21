de401b6aa319c6d6fbe7df7cfc52b82d53444ad6
Susi <susannebataloff@gmail.com>
2023-04-21 17:23:57 +0200

    update susi
    - added two smoke particles for vulcano
    -created new material for smoke
    -added mat to trees that were white
    -pushing marcels new animation controller and animation controller script

---
+++ FreseGameJam/Assets/Scripts/SimpleAnimationController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Loneflower
+{
+    [RequireComponent(typeof(Animator))]
+    public class SimpleAnimationController : MonoBehaviour
+    {
+        [Header("REFERENCES")]
+        [SerializeField] private CharacterController myController;
+        [SerializeField] private ThirdPersonMovement thirdPersonMovement;
+        private Animator animator;
+
+        private void Awake()
+        {
+            animator = this.GetComponent<Animator>();
+        }
+
+        private void FixedUpdate()
+        {
+            GroundCheck();
+            SetVelocity();
+        }
+
+        /// <summary>
+        /// We set the velocity on the Animation Controller
+        /// </summary>
+        private void SetVelocity()
+        {
+            Debug.Log(myController.velocity);
+            animator.SetFloat("xVelocity", thirdPersonMovement.speed);
+            animator.SetFloat("yVelocity", myController.velocity.y);
+        }
+
+        /// <summary>
+        /// We check if we are grounded
+        /// </summary>
+        private void GroundCheck()
+        {
+            animator.SetBool("isGrounded", myController.isGrounded);
+        }
+    }
+}
+
