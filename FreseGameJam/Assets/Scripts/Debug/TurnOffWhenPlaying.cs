f7d6d844e9a1fdef3dc6b8af943dd750ba38f9ff
Felix <f.mundschenk@web.de>
2023-04-24 13:02:27 +0200

    Felix Update
    - continued work on adjusted colliders for walking platforms to get rid of jittering wehn crossing them
    - created folder for all collider-prefabs to clean up prefab folder
    - addendum to TurnOffWhenPlaying to allow destructing a whole GO on load

--- FreseGameJam/Assets/Scripts/Debug/TurnOffWhenPlaying.cs
+++ FreseGameJam/Assets/Scripts/Debug/TurnOffWhenPlaying.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class TurnOffWhenPlaying : MonoBehaviour
 {
+    [SerializeField] bool _destroyThis = false;
     void Awake()
     {
         GetComponent<MeshRenderer>().enabled = false;
+
+        if (_destroyThis)
+        {
+            Destroy(gameObject);
+        }
     }
 }
