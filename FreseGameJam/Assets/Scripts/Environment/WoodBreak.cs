3b089d8ea37d62354e28c94c70eac7eb49465f48
Felix <f.mundschenk@web.de>
2023-05-10 17:24:30 +0200

    Felix Update
    - breakingObj can now have a second particle that plays either immediatly or after an delay (wood planks in water can now make breaking wood and water particles)
    - implemented water & wood breaks in all wood everywhere in the world
    - fixed a bunch of nasty Jan buggs...
    - 2 more crowns placed!

--- FreseGameJam/Assets/Scripts/Environment/WoodBreak.cs
+++ FreseGameJam/Assets/Scripts/Environment/WoodBreak.cs
@@ -7,8 +7,24 @@ public class WoodBreak : MonoBehaviour
 
     //[SerializeField] AudioSource[] arrayOfBreakingSounds;
     [SerializeField] ParticleSystem Wood;
+    [Space(10)]
+
+    [SerializeField] float _delayForOtherParicle = 0.0f; // allow eg wood to break before splashing water
+    [SerializeField] ParticleSystem _otherParticle;
+
 
     [SerializeField] bool _canBreak = true;
+
+    /// <summary>
+    /// If a "BreakingObj" script is attached as well, then overwrite the delay for the secondary particle with the breaktime from that script.
+    /// </summary>
+    private void Start()
+    {
+        if(GetComponent<BreakingObj>() != null)
+        {
+            _delayForOtherParicle = GetComponent<BreakingObj>().timeTillBreak; 
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -23,7 +39,17 @@ public class WoodBreak : MonoBehaviour
             {
                 Wood.Play();
             }
+
+            // play secondary optional particle effect at an optional delay:
+            if (_otherParticle != null)
+            {
+                Invoke("PlayOtherParticles", _delayForOtherParicle);
+            }
         }
+    }
+    void PlayOtherParticles()
+    {
+        _otherParticle.Play();
     }
 
   
